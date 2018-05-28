8c32805501696858b25e9e6eba49916bf8f0056c
jwilyandi19 <jasonwilyandi@yahoo.co.id>
2018-05-28 14:44:34 +0700

    +

--- app/User.php
+++ app/User.php
@@ -27,4 +27,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function course() {
+        return $this->hasMany(Course::class);  
+    }
 }
