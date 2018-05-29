68b4ae92d3eefd1f797079ab65e14d004cae629f
DESKTOP-J1DNFS8\lenovo <hfdbch@gmail.com>
2018-05-29 20:09:48 +0700

    css form soal

--- resources/views/teacher/createquestion.blade.php
+++ resources/views/teacher/createquestion.blade.php
@@ -7,37 +7,34 @@
 	<input type="hidden" name="questnum" value="{{$quest_num}}">
 	{{csrf_field()}}
 	@for($forms = 1; $forms <= $quest_num; $forms++)
-		<div>
-		<label for="question[{{$forms}}]">Question {{$forms}} </label>
-		<input name="question[{{$forms}}]">
+		<div class="form-group">
+			<label for="question[{{$forms}}]">Question {{$forms}} </label>
+			<input name="question[{{$forms}}]" class="form-control input-lg">
 		</div>
 		<div>
-		<label for="optiona[{{$forms}}]">A</label>
-		<input name="optiona[{{$forms}}]">
+			<label for="optiona[{{$forms}}]">A</label>
+			<input name="optiona[{{$forms}}]">
 		</div>
 		<div>
-		<label for="optionb[{{$forms}}]">B</label>
-		<input name="optionb[{{$forms}}]">
+			<label for="optionb[{{$forms}}]">B</label>
+			<input name="optionb[{{$forms}}]">
 		</div>
 		<div>
-		<label for="optionc[{{$forms}}]">C</label>
-		<input name="optionc[{{$forms}}]">
+			<label for="optionc[{{$forms}}]">C</label>
+			<input name="optionc[{{$forms}}]">
 		</div>
 		<div>
-		<label for="optiond[{{$forms}}]">D</label>
-		<input name="optiond[{{$forms}}]">
+			<label for="optiond[{{$forms}}]">D</label>
+			<input name="optiond[{{$forms}}]">
 		</div>
 		<div>
-		<label for="trueanswer[{{$forms}}]">True Answer</label>
-		<input name="trueanswer[{{$forms}}]">
-
-
-
+			<label for="trueanswer[{{$forms}}]">True Answer</label>
+			<input name="trueanswer[{{$forms}}]">
 		</div>
 
 	@endfor
 
-	<button type="submit">Submit</button>
+	<button type="submit"  class="wrapper button btn">Submit</button>
 
 
 	</form>
